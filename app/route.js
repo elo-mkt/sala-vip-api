cf9bf7a3ab6ee1c51183d4a381806f2b6b5dcb22
Eduardo Guilhon <eduardo@catskillet.com>
2019-01-21 12:59:34 -0200

    teste

--- app/route.js
+++ app/route.js
@@ -19,7 +19,7 @@ router.get('/teste', function (req, res, next) {
 //
 router.get('/sync', function (req, res, next) {
     res.set('Content-Type', 'application/json');
-
+    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
     request
         .get({
                 url: process.env.API_LOUNGE_URL_FACILITIES + '?lang=pt-br',
