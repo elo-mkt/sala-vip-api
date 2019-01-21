16db3854c3ba3663e8fb9fbf22c25a36ab2871b8
Eduardo Guilhon <eduardo@catskillet.com>
2019-01-21 09:23:20 -0200

    teste

--- app/route.js
+++ app/route.js
@@ -1,7 +1,7 @@
 var express = require('express'),
-    router = express.Router(),
-    cmd = require('./command'),
-    loungeApi = require('./services/apiExternaLounge');
+    router = express.Router();
+    // cmd = require('./command'),
+    // loungeApi = require('./services/apiExternaLounge');
 
 router.get('/', function (req, res, next) {
     res.set('Content-Type', 'application/json');
@@ -9,21 +9,21 @@ router.get('/', function (req, res, next) {
     res.send(JSON.stringify({a: 1}));
 });
 
-router.get('/teste', function (req, res, next) {
-    res.set('Content-Type', 'application/json');
-    cmd.getCountries(req, res);
+// router.get('/teste', function (req, res, next) {
+//     res.set('Content-Type', 'application/json');
+//     cmd.getCountries(req, res);
+//
+// });
 
-});
-
-router.get('/sync', function (req, res, next) {
-    res.set('Content-Type', 'application/json');
-    loungeApi();
-    res.send(JSON.stringify({'status': 1}));
-});
-
-router.get('/get-countries', function (req, res, next) {
-    cmd.getCountries(req, res)
-});
+// router.get('/sync', function (req, res, next) {
+//     res.set('Content-Type', 'application/json');
+//     loungeApi();
+//     res.send(JSON.stringify({'status': 1}));
+// });
+//
+// router.get('/get-countries', function (req, res, next) {
+//     cmd.getCountries(req, res)
+// });
 
 router.get('/pagecount', function (req, res, next) {
     res.set('Content-Type', 'application/json');
