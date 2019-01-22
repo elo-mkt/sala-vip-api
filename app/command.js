f5effd80db13f9f8e61027e29db107d7de8e72c7
Eduardo Guilhon <eduardo@catskillet.com>
2019-01-22 09:01:58 -0200

    teste

--- app/command.js
+++ app/command.js
@@ -1,9 +1,15 @@
 let mongodb = require('./config/mongodb');
 
 // Api end points...
-var command = {}
+let command = {}
 
+
+let mongodb = require('../../config/database');
+const fs = require('fs');
+
+// Api end points...
 command.getCountries = function (request, response) {
+
     mongodb(function (db) {
         db.collection('lounges').aggregate([
             {
@@ -17,10 +23,13 @@ command.getCountries = function (request, response) {
         ]).toArray(function (err, result) {
             return response.send(result);
         })
+
     });
+
 };
 
 command.getCitiesByCountry = function (request, response) {
+
     mongodb(function (db) {
         db.collection('lounges').aggregate([
             {
@@ -43,6 +52,7 @@ command.getCitiesByCountry = function (request, response) {
 };
 
 command.getLoungesByCityAndCountry = function (request, response) {
+
     mongodb(function (db) {
         db.collection('lounges').find(
             {id_city_slug: request.query.id_city_slug, id_country_slug: request.query.id_country_slug},
@@ -50,6 +60,7 @@ command.getLoungesByCityAndCountry = function (request, response) {
         ).toArray(function (err, result) {
             return response.send(result);
         });
+
     });
 };
 
@@ -58,7 +69,9 @@ command.getLoungesByCityAndCountry = function (request, response) {
 command.resetToken = function (request, response) {
     mongodb(function (db) {
         db.collection('lounge_token').deleteMany({}, function (error, result) {
+
             return response.send({result: 'token deletado com sucesso.'});
+
         });
     });
 
@@ -72,6 +85,112 @@ command.lastSync = function (request, response) {
             return response.send(result);
         })
     });
+
+};
+
+
+command.importFileJsonLoungeToken = function (request, response) {
+    var jsonData = fs.readFileSync("./export_lounge_token.json", "utf8");
+    var data = JSON.parse(jsonData);
+
+    mongodb(function (db) {
+        db.collection('lounge_token').deleteMany();
+        db.collection('lounge_token').insertOne({token: data.token, created_at: new Date()})
+    });
+
+
+    return response.send('ok');
+
+};
+
+command.importFileJsonLounge = function (request, response) {
+
+    var jsonData = fs.readFileSync("./lounges.json", "utf8");
+    var data = JSON.parse(jsonData);
+
+    mongodb(function (db) {
+        db.collection('lounges').deleteMany();
+    });
+
+    for (var item in data) {
+
+        mongodb(function (db) {
+
+            db.collection('lounges').insertOne({
+                dci_code: data[item].dci_code,
+                lang: 'pt-br',
+                facilities: data[item].facilities,
+                telephone: data[item].telephone,
+                fax: data[item].fax,
+                lounge_name: data[item].lounge_name,
+                airport_name: data[item].airport_name,
+                updated_at: data[item].updated_at,
+                lounge_status: data[item].lounge_status,
+                comments: data[item].comments,
+                guest_currency: data[item].guest_currency,
+                guest_fee_rate: data[item].guest_fee_rate,
+                fee_notes: data[item].fee_notes,
+                slug: data[item].slug,
+                provider: data[item].provider,
+                country: data[item].country,
+                city: data[item].city,
+                id_country_slug: data[item].country.toString().toLowerCase()
+                    .replace(/\s+/g, '-')           // Replace spaces with -
+                    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+                    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+                    .replace(/^-+/, '')             // Trim - from start of text
+                    .replace(/-+$/, ''),
+                id_city_slug: data[item].city.toString().toLowerCase()
+                    .replace(/\s+/g, '-')           // Replace spaces with -
+                    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+                    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+                    .replace(/^-+/, '')             // Trim - from start of text
+                    .replace(/-+$/, ''),
+                terminal: data[item].terminal,
+                location: data[item].location,
+                marketing_copy: data[item].marketing_copy,
+                opening_hours: data[item].opening_hours,
+                conditions: data[item].conditions,
+                additional: data[item].additional,
+                terminal_accessibility: data[item].terminal_accessibility,
+                created_at: new Date()
+            });
+
+        });
+    }
+
+
+    return response.send('ok');
+
+};
+
+command.importFileJsonLoungeFacilities = function (request, response) {
+    var jsonData = fs.readFileSync("./lounges_facilities.json", "utf8");
+    var data = JSON.parse(jsonData);
+
+
+    mongodb(function (db) {
+        db.collection('lounge_facilities').deleteMany();
+    });
+
+    for (var item in data) {
+
+        mongodb(function (db) {
+
+            db.collection('lounge_facilities').insertOne({
+                lang: data[item].lang,
+                token: data[item].token,
+                name: data[item].string,
+                created_at: new Date(),
+                updated_at: new Date()
+
+            })
+
+        });
+    }
+
+    return response.send('ok');
+
 };
 
 
--- app/route.js
+++ app/route.js
@@ -5,35 +5,88 @@ var express = require('express'),
     cmd = require('./command'),
     loungeApi = require('./services/apiExternaLounge');
 
-router.get('/', function (req, res, next) {
+
+//SYNC
+router.get('/sync1', function (req, res, next) {
     res.set('Content-Type', 'application/json');
-    // cmd.getCountries(req, res);
-    res.send(JSON.stringify({a: 1}));
+    cmd.importFileJsonLoungeToken(req, res);
 });
 
-router.get('/teste', function (req, res, next) {
+router.get('/sync2', function (req, res, next) {
     res.set('Content-Type', 'application/json');
-    cmd.getCountries(req, res);
-
+    cmd.importFileJsonLounge(req, res);
 });
-//
+
+router.get('/sync3', function (req, res, next) {
+    res.set('Content-Type', 'application/json');
+    cmd.importFileJsonLoungeFacilities(req, res);
+});
+
 router.get('/sync', function (req, res, next) {
     res.set('Content-Type', 'application/json');
-    request
-        .get({
-                url: process.env.API_LOUNGE_URL_FACILITIES + '?lang=pt-br',
-                rejectUnauthorized: false,
-                strictSSL: false
-            }
-        )
-        .on('response', function (response) {
-            res.send(JSON.stringify({'msg': response}));
-        });
-})
-;
+    cmd.importFileJsonLoungeToken(req, res);
+    cmd.importFileJsonLounge(req, res);
+    cmd.importFileJsonLoungeFacilities(req, res);
+});
 
+//APP
 router.get('/get-countries', function (req, res, next) {
     cmd.getCountries(req, res)
+});
+
+router.get('/get-cities-by-country', function (req, res, next) {
+    cmd.getCitiesByCountry(req, res)
+});
+
+router.get('/get-lounges-by-city-and-country', function (req, res, next) {
+    cmd.getLoungesByCityAndCountry(req, res)
+});
+
+router.get('/administrador/reset-token', function (req, res, next) {
+    cmd.resetToken(req, res)
+});
+
+router.get('/administrador/last-sync', function (req, res, next) {
+    cmd.lastSync(req, res)
+});
+
+
+router.get('/administrador/import-file-lounge-token-json', function (req, res, next) {
+    cmd.importFileJsonLoungeToken(req, res)
+});
+
+router.get('/adminsitrador/import-file-lounges-json', function (req, res, next) {
+    cmd.importFileJsonLounge(req, res)
+});
+
+router.get('/administrador/import-file-lounges-facilities', function (req, res, next) {
+    cmd.importFileJsonLoungeFacilities(req, res)
+});
+
+
+
+
+
+
+// //
+// router.get('/sync', function (req, res, next) {
+//     res.set('Content-Type', 'application/json');
+//     request
+//         .get({
+//                 url: process.env.API_LOUNGE_URL_FACILITIES + '?lang=pt-br',
+//                 rejectUnauthorized: false,
+//                 strictSSL: false
+//             }
+//         )
+//         .on('response', function (response) {
+//             res.send(JSON.stringify({'msg': response}));
+//         });
+// });
+
+//CONTROL
+router.get('/', function (req, res, next) {
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify({1: 1}));
 });
 
 router.get('/pagecount', function (req, res, next) {
